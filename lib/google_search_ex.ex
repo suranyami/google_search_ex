1d9742e563794576651eb1670415a4b6825a4ce1
David Parry <david.parry@industrieit.com>
2015-07-14 19:42:24 +1000

    made totally simple get_uri function

--- lib/google_search_ex.ex
+++ lib/google_search_ex.ex
@@ -1,2 +1,18 @@
 defmodule GoogleSearchEx do
+  
+  def get_uri(query) do
+    uri = "https://www.google.com/uds"
+    type = "web"
+    version = "v=1.0"
+    offset = "start=0"
+    size = "rsz=large"
+    language = "hl=en"
+    
+    "#{uri}/G#{type}Search?"
+      <> "q=#{URI.encode(query)}&"
+      <> "#{version}&"
+      <> "#{offset}&"
+      <> "#{size}&"
+      <> "#{language}"
+  end
 end
